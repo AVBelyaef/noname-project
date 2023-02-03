059e74c318c23305ebf91ef6054f77b9a40e0d45
Alexander Belyaev <belyaevav@sibedge.com>
2023-02-03 11:51:27 +0300

    webpack setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Counter } from './Components/Counter';
+import './index.scss'
+
+const App = () => {
+  return (
+    <div className='app'>
+      asdf
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
