6ab07098e196bc012ffc3d92505cbc142cc542e2
Alexander Belyaev <belyaevav@sibedge.com>
2023-02-04 22:11:46 +0300

    add themes & variables

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import { Routes, Route, Link } from 'react-router-dom';
 import { Suspense } from 'react';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
-import './index.scss';
+import './styles/index.scss';
+import { useTheme } from './theme/useTheme';
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle</button>
       <Link to={'/'}>Main</Link>
       <Link to={'/about'}>About</Link>
       <Suspense fallback={<div>Loading...</div>}>
