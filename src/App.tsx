67cf8fa5ac638a6d409a58f00d5ac2b988c0c61e
Alexander Belyaev <belyaevav@sibedge.com>
2023-02-04 15:18:21 +0300

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { Counter } from './Components/Counter';
-import './index.scss'
+import { Routes, Route } from 'react-router-dom';
+import AboutPage from './pages/AboutPage/AboutPage';
+import MainPage from './pages/MainPage/MainPage';
+import './index.scss';
+import { Link } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className='app'>
-      asdf
-      <Counter />
+    <div className="app">
+      <Link to={'/'}>Main</Link>
+      <Link to={'/about'}>About</Link>
+      <Routes>
+        <Route path={'/about'} element={<AboutPage />} />
+        <Route path={'/'} element={<MainPage />} />
+      </Routes>
     </div>
   );
 };
