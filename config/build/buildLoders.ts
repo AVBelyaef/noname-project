a032f87eafc590b860932b6b1b9455b8ee8e37b0
Alexander Belyaev <belyaevav@sibedge.com>
2023-02-18 11:39:08 +0300

    add babel & i18next-extract

--- config/build/buildLoders.ts
+++ config/build/buildLoders.ts
@@ -18,6 +18,26 @@ export function buildLoaders({ isDev }: BuildOptions): RuleSetRule[] {
     use: ['@svgr/webpack'],
   };
 
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+        plugins: [
+          [
+            'i18next-extract',
+            {
+              locales: ['ru', 'en'],
+              keyAsDefaultValue: true,
+            },
+          ],
+        ],
+      },
+    },
+  };
+
   const cssLoaders = {
     test: /\.s[ac]ss$/i,
     use: [
@@ -43,5 +63,11 @@ export function buildLoaders({ isDev }: BuildOptions): RuleSetRule[] {
     exclude: /node_modules/,
   };
 
-  return [typesciptLoader, cssLoaders, svgLoader, fileLoader];
+  return [
+    fileLoader,
+    svgLoader,
+    babelLoader,
+    typesciptLoader,
+    cssLoaders,
+  ];
 }
