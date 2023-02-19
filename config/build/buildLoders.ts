30e1a56ac240c53d0a88678b7baa3f1823dabeff
Alexander Belyaev <belyaevav@sibedge.com>
2023-02-19 22:43:53 +0300

    return buildCssLoader

--- config/build/buildLoders.ts
+++ config/build/buildLoders.ts
@@ -1,22 +1,10 @@
-import { RuleSetRule } from 'webpack';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import webpack from 'webpack';
+import { buildCssLoader } from './loaders/buildCssLoader';
 import { BuildOptions } from './types/config';
-// FIXME: вынести, разобраться почему валиться build в github actions
-// import { buildCssLoader } from './loaders/buildCssLoader';
 
-export function buildLoaders({ isDev }: BuildOptions): RuleSetRule[] {
-  const fileLoader = {
-    test: /\.(png|jpe?g|gif|woff2|woff)$/i,
-    use: [
-      {
-        loader: 'file-loader',
-      },
-    ],
-  };
-
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
   const svgLoader = {
-    test: /\.svg$/i,
-    issuer: /\.[jt]sx?$/,
+    test: /\.svg$/,
     use: ['@svgr/webpack'],
   };
 
@@ -40,36 +28,29 @@ export function buildLoaders({ isDev }: BuildOptions): RuleSetRule[] {
     },
   };
 
-  const cssLoaders = {
-    test: /\.s[ac]ss$/i,
-    use: [
-      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-      {
-        loader: 'css-loader',
-        options: {
-          modules: {
-            auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-            localIdentName: isDev
-              ? '[path][name]__[local]--[hash:base64:5]'
-              : '[hash:base64:8]',
-          },
-        },
-      },
-      'sass-loader',
-    ],
-  };
+  const cssLoader = buildCssLoader(isDev);
 
-  const typesciptLoader = {
+  // Если не используем тайпскрипт - нужен babel-loader
+  const typescriptLoader = {
     test: /\.tsx?$/,
     use: 'ts-loader',
     exclude: /node_modules/,
+  };
+
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+    use: [
+      {
+        loader: 'file-loader',
+      },
+    ],
   };
 
   return [
     fileLoader,
     svgLoader,
     babelLoader,
-    typesciptLoader,
-    cssLoaders,
+    typescriptLoader,
+    cssLoader,
   ];
 }
