54a30c1089c438bed891228aca125281e725beff
Alexander Belyaev <belyaevav@sibedge.com>
2023-03-04 17:49:34 +0300

    add login module

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -21,6 +21,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   theme?: ButtonTheme;
   square?: boolean;
   size?: ButtonSize;
+  disabled?: boolean;
 }
 
 export const Button: FC<ButtonProps> = (props) => {
@@ -30,11 +31,13 @@ export const Button: FC<ButtonProps> = (props) => {
     theme,
     square,
     size,
+    disabled,
     ...otherProps
   } = props;
 
   const mods: Record<string, boolean> = {
     [cls.square]: square,
+    [cls.disabled]: disabled,
   };
 
   return (
@@ -43,6 +46,7 @@ export const Button: FC<ButtonProps> = (props) => {
       className={
         classNames(cls.Button, mods, [className, cls[theme], cls[size]])
       }
+      disabled={disabled}
       {...otherProps}
     >
       {children}
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -4,6 +4,8 @@ import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { LoginModal } from 'features/AuthByUsername';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserAuthData, userActions } from 'entities/User';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -13,6 +15,8 @@ interface NavbarProps {
 export function Navbar({ className }: NavbarProps) {
   const [isAuthModal, setIsAuthModal] = useState(false);
   const { t } = useTranslation();
+  const authData = useSelector(getUserAuthData);
+  const dispatch = useDispatch();
 
   const onCloseModal = useCallback(() => {
     setIsAuthModal(false);
@@ -21,6 +25,24 @@ export function Navbar({ className }: NavbarProps) {
   const onShowModal = useCallback(() => {
     setIsAuthModal(true);
   }, []);
+
+  const onLogout = useCallback(() => {
+    dispatch(userActions.logout());
+  }, [dispatch]);
+
+  if (authData) {
+    return (
+      <div className={classNames(cls.Navbar)}>
+        <Button
+          className={cls.links}
+          theme={ButtonTheme.CLEAR_INVERTED}
+          onClick={onLogout}
+        >
+          {t('Выйти')}
+        </Button>
+      </div>
+    );
+  }
 
   return (
     <div className={classNames(cls.Navbar)}>
